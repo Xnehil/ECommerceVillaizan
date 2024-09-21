a5e528754a9e4084646183cab7ed0e3656f93402
Jose <a20190202@pucp.edu.pe>
2024-09-21 16:30:46 -0500

    skill issue de harvy

--- backendecommerce/src/models/EntidadBase.ts
+++ backendecommerce/src/models/EntidadBase.ts
@@ -5,18 +5,18 @@ export abstract class EntidadBase{
     @PrimaryColumn({ type: "varchar", length: 50 })
     id: string;
 
-    @CreateDateColumn({ type: "timestamp" })
+    @CreateDateColumn({ type: "timestamp", name: "creadoen"})
     creadoEn: Date;
 
-    @UpdateDateColumn({ type: "timestamp" })
+    @UpdateDateColumn({ type: "timestamp" , name: "actualizadoen"})
     actualizadoEn: Date;
 
-    @DeleteDateColumn({ type: "timestamp", nullable: true })
+    @DeleteDateColumn({ type: "timestamp", name: "desactivadoen", nullable: true })
     desactivadoEn: Date | null;
 
-    @Column({ type: "varchar", length: 50 })
+    @Column({ type: "varchar", name: "usuariocreacion", length: 50 })
     usuarioCreacion: string;
 
-    @Column({ type: "varchar", length: 50, nullable: true })
+    @Column({ type: "varchar", name: "usuarioactualizacion", length: 50, nullable: true })
     usuarioActualizacion: string;
 }
--- backendecommerce/src/models/LibroReclamaciones.ts
+++ backendecommerce/src/models/LibroReclamaciones.ts
@@ -12,7 +12,7 @@ import { EntidadBase } from "./EntidadBase"
 
 @Entity("vi_libro_reclamaciones") 
 export class LibroReclamaciones extends EntidadBase {
-    @Column({ type: "timestamp" })
+    @Column({ type: "timestamp", name: "fechaincidente" })
     fechaIncidente: Date
 
     @Column({ type: "varchar", length: 100 })
@@ -27,10 +27,10 @@ export class LibroReclamaciones extends EntidadBase {
     @Column({ type: "varchar", length: 20, nullable: true })
     telefono: string
 
-    @Column({ type: "varchar", length: 3 })
+    @Column({ type: "varchar", length: 3, name: "tipodoc" })
     tipoDoc: string
 
-    @Column({ type: "varchar", length: 20 })
+    @Column({ type: "varchar", length: 20, name: "nrdocumento" })
     nrDocumento: string
 
     @Column({ type: "boolean", default: false })
@@ -51,7 +51,7 @@ export class LibroReclamaciones extends EntidadBase {
     @Column({ type: "varchar", length: 10 })
     tipo: string
 
-    @Column({ type: "decimal", precision: 10, scale: 2 })
+    @Column({ type: "decimal", precision: 10, scale: 2, name: "montoreclamado" })
     montoReclamado: number
 
     @Column("text")
@@ -60,31 +60,31 @@ export class LibroReclamaciones extends EntidadBase {
     @Column({ type: "varchar", length: 7 })
     reclamacion: string
 
-    @Column({ type: "varchar", length: 50, nullable: true })
+    @Column({ type: "varchar", length: 50, nullable: true, name: "nrpedido" })
     nrPedido: string
 
-    @Column({ type: "timestamp", nullable: true })
+    @Column({ type: "timestamp", nullable: true, name: "fechapedido" })
     fechaPedido: Date
 
     @Column("text", { nullable: true })
     detalle: string
 
-    @Column("text", { nullable: true })
+    @Column("text", { nullable: true, name: "pedidoconcreto" })
     pedidoConcreto: string
 
-    @Column({ type: "varchar", length: 200, nullable: true })
+    @Column({ type: "varchar", length: 200, nullable: true, name: "urlcomprobante" })
     urlComprobante: string
 
-    @Column("text", { nullable: true })
+    @Column("text", { nullable: true, name: "accionesproveedor" })
     accionesProveedor: string
 
-    @Column({ type: "boolean", default: true })
+    @Column({ type: "boolean", default: true, name: "estaactivo" })
     estaActivo: boolean
 
-    @Column({ type: "varchar", length: 50 })
+    @Column({ type: "varchar", length: 50, name: "usuariocreacion" })
     usuarioCreacion: string
 
-    @Column({ type: "varchar", length: 50, nullable: true })
+    @Column({ type: "varchar", length: 50, nullable: true, name: "usuarioactualizacion" })
     usuarioActualizacion: string
 
     @BeforeInsert()
