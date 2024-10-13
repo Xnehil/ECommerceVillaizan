c4973134ab5821a104f00f04b09b990df3c350e0
Amy Trujillo <a20201521@pucp.edu.pe>
2024-10-12 19:19:33 -0500

    faltaba este

--- adminfront/src/components/forms/textAreaWithLabel.tsx
+++ adminfront/src/components/forms/textAreaWithLabel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 
