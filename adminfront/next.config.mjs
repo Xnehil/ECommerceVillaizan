d7a6b51020451296caaf991976390706c817904e
Xnehil <harvy.martinezm@pucp.edu.pe>
2024-10-13 10:05:24 -0500

    Cambio back url

--- adminfront/next.config.mjs
+++ adminfront/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_BASE_URL: 'http://localhost:9000/admin/',
+    NEXT_PUBLIC_BASE_URL: 'https://back.heladosvillaizan.tech/admin/',
   },
 };
 
