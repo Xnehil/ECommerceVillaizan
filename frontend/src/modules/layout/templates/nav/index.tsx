667c0f728d6d5160c743f0073b86aa23b4191fa2
Jose <a20190202@pucp.edu.pe>
2024-10-27 09:30:40 -0500

    display name

--- frontend/src/modules/layout/templates/nav/index.tsx
+++ frontend/src/modules/layout/templates/nav/index.tsx
@@ -3,39 +3,39 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useSession } from "next-auth/react";
-import { Loader2 } from "lucide-react";
 import { Button } from "@components/Button";
 import { signOut } from "next-auth/react";
-import { getToken } from "next-auth/jwt";
-import jwt from "jsonwebtoken";
+import axios from 'axios';
 
 export async function handleSignOut() {
   await signOut();
 }
 
 export default function Nav() {
-  const [loginUrl, setLoginUrl] = useState('');
   const { data: session, status } = useSession();
+  const [userName, setUserName] = useState('');
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  /*
-  useEffect(() => {
-    const currentUrl = window.location.href;
-    setLoginUrl(`http://localhost:3000/login?redirect=${encodeURIComponent(currentUrl)}`);
-  }, []);
-  */
 
   useEffect(() => {
-    
-    if (session) {
-      console.log('Session:', session);
-      console.log('id:', session.user?.id); // Log the user name
-      console.log('User name:', session.user?.name); // Log the user name
-      console.log('email:', session.user?.email); // Log the user name
-      console.log('status:', status); // Log the user name
-      //console the cookie
-      console.log('cookie:', document.cookie);
+    async function fetchUserName() {
+      if (session?.user?.id) {
+        try {
+          const response = await axios.get(`${process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL}/admin/usuario/${session.user.id}`);
+          console.log("response", response);
+          const user = response.data.usuario;
+          if (user) {
+            setUserName(user.nombre);  // Assuming the backend returns { name: "User Name" }
+          } else {
+            console.error('Failed to fetch user name');
+          }
+        } catch (error) {
+          console.error('Error fetching user name:', error);
+        }
+      }
     }
-  }, [session, status]);
+
+    fetchUserName();
+  }, [session]);
 
   return (
     <div className="sticky top-0 inset-x-0 z-50 bg-rojoVillaizan">
@@ -60,37 +60,21 @@ export default function Nav() {
             <Link href="/comprar" className="hover:text-ui-fg-base text-white">
               Comprar
             </Link>
-
-            {/*Antiguo Iniciar Sesión*/}
-            {/*<a href={loginUrl} className="hover:text-ui-fg-base text-white flex items-center">
-              Inicia Sesión y accede a promociones
-            </a> 
-
-            <div className="z-[51] hidden items-center space-x-2 md:flex">
-              {status === "loading" ? (
-                <Loader2 className="h-4 w-4 animate-spin mr-3"></Loader2>
-              ) : session ? (
-                <Button className="text-lg font-bold" onClick={() => handleSignOut()} variant="link">
-                  Cerrar sesión
-                </Button>
-              ) : (
-                <a href={loginUrl} className="hover:text-ui-fg-base text-white flex items-center">
-                  Inicia Sesión y accede a promociones
-                </a>
-              )}
-            </div> */}
             {status === "loading" ? (
               <Button isLoading loaderClassname="w-6 h-6" variant="ghost"></Button>
             ) : session ? (
-              <Button
-                className="text-lg text-white"
-                onClick={() => {
-                  handleSignOut();
-                  setIsMobileMenuOpen(false);
-                }}
-              >
-                Cerrar sesión
-              </Button>
+              <>
+                <span className="text-lg text-white">Bienvenido, {userName}</span>
+                <Button
+                  className="text-lg text-white"
+                  onClick={() => {
+                    handleSignOut();
+                    setIsMobileMenuOpen(false);
+                  }}
+                >
+                  Cerrar sesión
+                </Button>
+              </>
             ) : (
               <Button className="text-lg text-white">
                 <Link
