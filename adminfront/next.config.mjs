b968d97e902f0066128c4d1b08f71d0f27acac85
Xnehil <harvy.martinezm@pucp.edu.pe>
2024-10-31 16:02:10 -0500

    Solo master next.config

--- adminfront/next.config.mjs
+++ adminfront/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_BASE_URL: 'http://localhost:9000/admin/',
-    NEXT_PUBLIC_WS_URL: 'ws://localhost:9001/ws/',
+    NEXT_PUBLIC_BASE_URL: 'https://back.heladosvillaizan.tech',
+    NEXT_PUBLIC_WS_URL: 'wss://back.heladosvillaizan.tech',
     AUTH_SECRET: "9st1Wnz+sLYAuTyk9tfiggU/lYJzBHueNUtqpRkhPZs="
   },
   images: {
