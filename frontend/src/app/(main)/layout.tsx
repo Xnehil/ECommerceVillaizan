23e8a2672473d0126db3802b140510fb79857330
Xnehil <harvy.martinezm@pucp.edu.pe>
2024-11-12 12:14:22 -0500

    Chatbot parches visuales

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -5,6 +5,8 @@ import { SessionProvider } from "next-auth/react";
 import Footer from "@modules/layout/templates/footer";
 import Nav from "@modules/layout/templates/nav";
 import Chatbot from "@components/Chatbot";
+import path from "path";
+import fs from "fs";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000";
 
@@ -13,12 +15,16 @@ export const metadata: Metadata = {
 };
 
 export default function PageLayout(props: { children: React.ReactNode }) {
+  const cssPath = path.join(process.cwd(), 'src/styles/asistente.css');
+  const cssContent = fs.readFileSync(cssPath, 'utf-8');
+  const base64Css = Buffer.from(cssContent).toString('base64');
+
   return (
     <SessionProvider>
       <Nav />
       {props.children}
       <Footer />
-      <Chatbot />
+      <Chatbot base64Css={base64Css} />
     </SessionProvider>
   );
 }
