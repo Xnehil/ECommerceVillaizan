a75024275951a2b0ad6a3328bd7b74d82043edfd
Xnehil <harvy.martinezm@pucp.edu.pe>
2024-11-19 23:15:52 -0500

    Api deploy

--- adminfront/next.config.mjs
+++ adminfront/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_BASE_URL: "http://localhost:9000/admin/",
-    NEXT_PUBLIC_WS_URL: "ws://localhost:9001/ws/",
+    NEXT_PUBLIC_BASE_URL: 'https://back.heladosvillaizan.tech/admin/',
+    NEXT_PUBLIC_WS_URL: 'wss://back.heladosvillaizan.tech/ws/',
     AUTH_SECRET: "9st1Wnz+sLYAuTyk9tfiggU/lYJzBHueNUtqpRkhPZs=",
     NEXT_PUBLIC_APP_URL:"https://heladosvillaizan.tech",
     NEXT_PUBLIC_MEDUSA_BACKEND_URL:"https://back.heladosvillaizan.tech",
